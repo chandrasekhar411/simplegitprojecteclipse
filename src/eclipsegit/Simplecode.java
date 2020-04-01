25b3203738e601dc6728735022525e7ea4a846ca
subbareddy <subbufantastic1234@gmail.com>
2020-04-01 11:06:55 +0530

    This is first commit

---
+++ src/eclipsegit/Simplecode.java
@@ -0,0 +1,15 @@
+package eclipsegit;
+
+public class Simplecode {
+
+	public static void main(String[] args) {
+		String s="hungry";
+		if(s=="hungry"){
+			System.out.println("Eat something");
+		}
+		else{
+			System.out.println("Do your own work");
+		}
+	}
+
+}
